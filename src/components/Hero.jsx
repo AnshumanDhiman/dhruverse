335586acbd0993fb437850ff41c3d7844fde9a4b
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-11-07 01:42:38 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,15 @@ const HeroSection = () => {
   return (
     <>
       <div className="relative lg:h-[35rem] md:h-[25rem] ">
-  <video
-    className="lg:w-full md:w-full  lg:h-full md:h-full object-cover"
-    src={vid}
-    autoPlay
-    loop
-    muted
-    style={{ objectPosition: 'center 20%' }}
-  />
+      <video
+  className="lg:w-full md:w-full lg:h-full md:h-full object-cover"
+  src={vid}
+  autoPlay
+  loop
+  muted
+  playsInline
+  style={{ objectPosition: 'center 20%' }}
+/>
 </div>
 
     </>
