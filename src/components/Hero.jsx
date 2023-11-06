516d1d52db54719905326b8958c7bc4bcabf9d47
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-11-07 01:49:59 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import vid from "../assets/video/video1.mp4";
 const HeroSection = () => {
   return (
     <>
-      <div className="relative lg:h-[35rem] md:h-[25rem] top-4 lg:top-0">
+      <div className="relative  lg:h-[35rem] md:h-[25rem] top-4 lg:top-0">
       <video
-  className="lg:w-full md:w-full lg:h-full md:h-full object-cover"
+  className="lg:w-full md:w-full lg:h-full md:h-full h-[16.5rem] object-cover"
   src={vid}
   autoPlay
   loop
