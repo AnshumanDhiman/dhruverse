9cfe222a2c367749d49a3c420c30c479b1efa917
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-11-07 02:13:28 +0530

    new

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -29,16 +29,16 @@ const OurServices = () => {
           New Arrivals
         </h2>
         <div className="lg:py-4 lg:px-32 lg:ml-[-390px]">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 rounded-lg">
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 rounded-lg">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-2 lg:gap-8">
               {services.map((service, index) => (
                 <div
                   key={index}
-                  className="flex flex-col items-center justify-center bg-theme_1 rounded-lg shadow-2xl transition duration-300 ease-in-out transform cursor-pointer hover:animate-pulse "
+                  className="flex flex-col items-center justify-center bg-theme_1 shadow-2xl transition duration-300 ease-in-out transform cursor-pointer hover:animate-pulse "
                 >
                   <img
                     src={service.image}
-                    className="rounded-lg "
+                    className=""
                     alt="image"
                   />
                 </div>
