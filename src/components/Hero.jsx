f0dab3d93936fc3d90e7abb7df1ba6865c5f6ace
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-11-10 00:34:17 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import vid from "../assets/video/video2.mp4";
+import ReactPlayer from 'react-player'
 const HeroSection = () => {
   return (
     <>
       <div className="relative lg:h-[35rem] md:h-[25rem] top-12 lg:top-0 lg:mb-0 mb-12">
-        <video
+      <ReactPlayer url={vid}   muted={true} playing={true} width='100%'
+          height='100%'/>
+        {/* <video
           className="lg:w-full md:w-full lg:h-full md:h-full object-cover"
           src={vid}
           autoPlay
@@ -11,7 +14,7 @@ const HeroSection = () => {
           muted
           playsInline
           style={{ objectPosition: "center 20%" }}
-        />
+        /> */}
       </div>
     </>
   );
