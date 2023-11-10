6ffe2a795616c6f6cc771b2c7b90fb2588c255f6
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-11-10 11:51:09 +0530

    patch#1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import vid from "../assets/video/video2.mp4";
+
 import ReactPlayer from 'react-player'
 const HeroSection = () => {
   return (
     <>
       <div className="relative lg:h-[35rem] md:h-[25rem] top-12 lg:top-0 lg:mb-0 mb-12">
-      <ReactPlayer url={vid}   muted={true} playing={true} width='100%'
+      <ReactPlayer url="/video2.mp4"   muted={true} playing={true} width='100%'
           height='100%'/>
         {/* <video
           className="lg:w-full md:w-full lg:h-full md:h-full object-cover"
