a1238b06ff72d65895b731ab1ec7d1e97449e948
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-12-30 15:43:49 +0530

    change ad

--- src/components/Cards/Ads/ACards.jsx
+++ src/components/Cards/Ads/ACards.jsx
@@ -8,17 +8,16 @@ import {
 import "./car.css";
 /* Install pure-react-carousel using -> npm i pure-react-carousel */
 
-import ai1 from "../../../assets/thumbnails/ads/1.webp";
+import ai1 from "../../../assets/thumbnails/ads/2.webp";
 import ai2 from "../../../assets/thumbnails/ads/10.jpeg";
-import ai3 from "../../../assets/thumbnails/ads/2.webp";
-import ai7 from "../../../assets/thumbnails/ads/3.webp";
+import ai3 from "../../../assets/thumbnails/ads/1.webp";
 import ai4 from "../../../assets/thumbnails/ads/4.webp";
 import ai5 from "../../../assets/thumbnails/ads/5.webp";
 import ai6 from "../../../assets/thumbnails/ads/6.webp";
-import ai10 from "../../../assets/thumbnails/ads/7.webp";
+import ai7 from "../../../assets/thumbnails/ads/3.webp";
 import ai8 from "../../../assets/thumbnails/ads/8.webp";
 import ai9 from "../../../assets/thumbnails/ads/9.webp";
-
+import ai10 from "../../../assets/thumbnails/ads/7.webp";
  
 export default function Index() {
   return (
